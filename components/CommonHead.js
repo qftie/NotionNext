3bc39ec5a92f9264b3de447c4bb90e6777d6e434
qftie <54592017+qftie@users.noreply.github.com>
2023-09-07 13:41:33 +0800

    Update CommonHead.js

--- components/CommonHead.js
+++ components/CommonHead.js
@@ -41,7 +41,6 @@ const CommonHead = ({ meta, children }) => {
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:description" content={description} />
             <meta name="twitter:title" content={title} />
-              
 
             {BLOG.COMMENT_WEBMENTION.ENABLE && (
                 <>
